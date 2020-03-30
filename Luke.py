d34b91d91f031edf14b153a713956db65436e994
Luke Ogle <slogle@protonmail.ch>
2020-03-30 00:40:14 -0500

    I created a write.py file that uses a function to correctly format the smoothed/normalized data, and then write it to a text file. The user also can name the file, write the name of the star, and choose how many data points to list...

--- Luke.py
+++ Luke.py
@@ -1,55 +1,21 @@
 # Importing Modules
-from PyAstronomy import pyasl
-import matplotlib.pyplot as plt
-import math as m
-import numpy as np
-from scipy.interpolate import UnivariateSpline
-import csv
-import os
-from astropy.io import fits
-from decimal import Decimal
-
 import norm as nm
 import smooth as sm
 import readfits as rdf
 import plot as pt
+import write as wt
 
 #Open and read the fits file
 wavelength, flux = rdf.readfits()
 
-
-x = wavelength
-y = flux
-
 #Normalizing
 norm_flux = nm.norm(flux)
-
-
-
 
 #Smoothing
 smooth_wavelength, smooth_flux = sm.smooth(wavelength, norm_flux)
 
-
 #Plotting the data
 pt.plot(wavelength, flux, smooth_wavelength, smooth_flux)
 
-#format(new_wavelength, '.10f')
-#format(new_flux, '.10f')
-
 #Writing to Text file
-L=len(new_flux)
-def form_e(q):
-    a = '%E' % q
-    return a.split('E')[0].rstrip('0').rstrip('0').rstrip('.')+'E'+a.split('E')[1]
-
-
-with open('Luke2020', 'w') as f:
-    f.write("HD6268 (Flux) (Lin           npts="+str(L) +"\n")
-    for i in range(0, L):
-        f.write("%6s" % ("")) 
-        f.write("%8s" % (form_e(new_wavelength[i])))
-        f.write("%20s" % (form_e(new_flux[i])))
-        f.write("\n")
-
-
+wt.write(smooth_wavelength, smooth_flux)
